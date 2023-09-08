fc09c9f88cd9f02f0978bf10a7ce1a3f8ef9a1a3
Terry Amato <terence.amato@terumobct.com>
2023-09-08 10:03:31 -0600

    starting zig

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,24 +1,77 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = gpa.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try bw.flush(); // don't forget to flush!
+    std.debug.print("Arguments: {s}\n", .{args[1]});
+
+    if (args.len < 2) {
+        return error.InvalidNumberOfArguments;
+    }
+
+    // load up a rom
+    const file_path = args[1];
+    const rom = try std.fs.openFileAbsolute(file_path, .{} );
+    defer rom.close();
+    
+    xxd(rom, file_path);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn xxd(rom: std.fs.File, file_name: []const u8) void {
+    _ = file_name;
+    var buf: [std.mem.page_size]u8 = undefined;
+    const reader = rom.reader();
+    while (true) {
+        const amt_read = reader.read(buf[0..]) catch |err| {
+            std.debug.print("Error, {any}", .{err});
+            return;
+        };
+
+        if (0 == amt_read) break;
+
+        for (buf[0..amt_read]) |byte| {
+            std.debug.print("byte: {x}\n", .{byte});
+        }
+    }
 }
+
+// fn xxd(rom: &Vec<u8>, file_name: String) {//{{{
+//     let mut disassemed_file = file_name;
+//     disassemed_file.push_str(".xxd");
+//     let mut diss = File::create(disassemed_file).unwrap();
+//
+//     let sz = rom.len();
+//     let mut bytes_read:usize = 1;
+//
+//     let mut row_counter:u32 = 0;
+//     let mut column_counter = 0;
+//     let col_byte_limit = 16;
+//
+//     let mut row_string = String::new();
+//     for byte in rom {
+//         let hex = to_hex(*byte, 2);
+//         row_string.push_str(&hex);
+//         column_counter += 1;
+//
+//         // write out the completed line.
+//         if    (column_counter == col_byte_limit)
+//            || (bytes_read == sz) {
+//             column_counter = 0;
+//
+//             let row_hex = format!("{row_counter:08X}");
+//             row_counter += col_byte_limit;
+//
+//             row_string = format!("0x{row_hex}: {row_string}\n");
+//             diss.write_all(row_string.as_bytes()).unwrap();
+//             row_string.clear();
+//         }
+//
+//         bytes_read += 1;
+//     }
+// }//}}}
